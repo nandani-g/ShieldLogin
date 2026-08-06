a5cad27dfcbc9dec06d2362dabd77896f9ae31f1
nandani-g <nandaniigupta02@gmail.com>
2026-08-06 23:03:18 +0530

    Update server.js to integrate new modules

--- client/server/server.js
+++ client/server/server.js
@@ -4,6 +4,9 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import { connect } from "mongoose";
+import connectDB from "./config/mongodb.js";
+import authRouter from './routes/authRoutes.js'
 
 
 
@@ -11,9 +14,17 @@ const app = express();
 
 const port = process.env.PORT || 4000
 
+connectDB();
+
 app.use(express.json());
 
 app.use(cookieParser());
 app.use(cors({credentials: true}));
+//API Endpoints
+app.get('/', (req,res)=> res.send("API Working fine"));
+
+app.use('/api/auth', authRouter)
+
+
 app.listen(port, ()=>
 console.log(`Server started on PORT : ${port}`));
