565d331bad572c6736ebcc2b50aa45da7a453abd
nandani-g <nandaniigupta02@gmail.com>
2026-08-07 23:33:51 +0530

    Update auth controller to support email verification

--- client/server/controller/authController.js
+++ client/server/controller/authController.js
@@ -3,123 +3,194 @@ import bcrypt from 'bcryptjs';
 
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
-import { use } from 'react';
 
+import transporter from '../config/nodemailer.js'
 // generated a token for authecation
 
 
-export const register = async (req,res)=>{
-  const {name, email, password} = req.body;
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
 
-
-  if(!name || !email || !password){
-    return res.json({success: false, message: 'Missing Details'})
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: 'Missing Details' })
   }
 
   try {
 
-    const existingsUser = await userModel.findOne({email})
+    const existingsUser = await userModel.findOne({ email })
 
-    if(existingsUser){
-       return res.json({success: false, message: "User already exists"});
+    if (existingsUser) {
+      return res.json({ success: false, message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = new userModel({name, email, password: hashedPassword});
+    const user = new userModel({ name, email, password: hashedPassword });
+
+    await user.save();
+
+    // now generated token by using JWT
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+// Sending welcome email
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: 'Welcome to SheildLogin',
+      text: `Welcome to SheildLogin website. Your account has been created with email id: ${email}`
+    }
+
+
+    await transporter.sendMail( mailOptions );
+
+    return res.json({ success: true });
+
+  } catch (error) {
+    res.json({ success: false, message: error.message })
+  }
+}
+
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({ success: false, message: 'Email and password are required' })
+  }
+
+  try {
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: 'Invalid email' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid password" })
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    return res.json({ success: true });
+
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+}
+
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie('token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    })
+
+    return res.json({ success: true, message: "Logged Out" })
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+}
+
+// Send Verification OTP to the User's Email
+
+export const sendVerifyOtp = async (req,res) => {
+  try {
+    const {userId} = req.body;
+    const user = await userModel.findById(userId);
+
+
+    if(user.isAccountVerified){
+      return res.json({success: false, message: "Account Already verified"})
+    }
+
+     const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+
+     user.sendVerifyOtp = otp;
+
+     user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000
+
+
+     await user.save();
+
+
+     const mailOptions = {
+     from: process.env.SENDER_EMAIL,
+      to: user.email,
+      subject: 'Account Verification OTP',
+      text: `Your OTP is ${otp}. Verify your account using this OTP.`
+     }
+
+
+     await transporter.sendMail(mailOptions);
+
+     res.json({success: true, message: 'Verification OTP Sent on Email'});
+
+
+  } catch (error) {
+    res.json({ success: false, message: error.message});
+  }
+}
+
+
+
+export const verifyEmail = async(req, res)=> {
+  const {userId,otp} = req.body;
+
+  if(!userId || !otp){
+    return res.json({success: false, message: "Missing Details"});
+  }
+  try {
+
+    const user = await userModel.findById(userId);
+
+
+    if(!user){
+      return res.json({success: false, message: 'User not found'});
+    }
+
+    if(user.verifyOtp === '' || user.verifyOtp !== otp){
+      return res.json({success: false, message: 'Invalid OTP'});
+    }
+
+
+    if(user.verifyOtpExpireAt < Date.now()){
+      return res.json({success: false, message: 'OTP Expired'})
+    }
+
+    user.isAccountVerified =  true;
+    user.verifyOtp = '';
+    user.verifyOtpExpireAt = 0;
 
 
     await user.save();
 
 
-    // now generated token by using JWT
-
-    const token = jwt.sign({id: user._id},process.env.JWT_SECRET, {expiresIn: '7d'});
-
-
-
-    res.cookie('token',token,  {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    });
-
-
-      return res.json({success: true});
-
-  } catch(error){
-    res.json({success: false, message: error.message})
-  }
-}
-
-
-
-
-
-
-export const login = async (req, res)=> 
-  {
-    const {email, password} = req.body;
-
-    if(!email || !password){
-      return res.json({success: false, message: 'Email and password are required'})
-    }
-
-    try {
-
-
-      const user = await userModel.findOne({email});
-
-
-      if(!user){
-        return res.json({success: false, message: 'Invalid email'});
-      }
-
-
-      const isMatch = await bcrypt.compare(password, user.password)
-
-      if(!isMatch){
-        return res.json({success: false, message: "Invalid password"})
-      }
-
-      
-    const token = jwt.sign({id: user._id},process.env.JWT_SECRET, {expiresIn: '7d'});
-
-
-
-    res.cookie('token',token,  {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    });
-
-
-
-    return res.json({success: true});
-
-    } catch(error){
-      return res.json({success: false, message: error.message});
-    }
-}
-
-
-
-
-
-export const logout = async()=> {
-  try {
-    res.clearCookie('token',{
-        httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-
+    return res.json({
+      success: true, message: "Email verified successfully"
     })
-
-    return res.json({success: true, message: "Logged Out"})
+    
   } catch (error) {
     return res.json({success: false, message: error.message});
-    
   }
 }
