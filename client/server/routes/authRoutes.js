b4c078cba7bdc3029a8e656a89dd0a30ee95770b
nandani-g <nandaniigupta02@gmail.com>
2026-08-08 23:29:07 +0530

    Update auth routes to apply middleware protection

--- client/server/routes/authRoutes.js
+++ client/server/routes/authRoutes.js
@@ -1,5 +1,12 @@
+
+// Route - when this URL is requested with this method, run this function
+//  Endpoint = the URL address (/api/auth/login)
+// Route = the actual code that listens for that endpoint and decides what to do
+
 import express from 'express';
-import { login, logout, register } from '../controller/authController.js';
+import { login, logout, register, sendVerifyOtp, verifyEmail, isAuthenticated, sendResetOtp, resetPassword } from '../controller/authController.js';
+import userAuth from '../middleware/userauth.js';
+
 
 
 const authRouter = express.Router();
@@ -7,6 +14,11 @@ const authRouter = express.Router();
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
-
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/verify-account', userAuth, verifyEmail);
+authRouter.post('/is-auth', userAuth, isAuthenticated);
+authRouter.post('/send-reset-otp', sendResetOtp);
+authRouter.post('/reset-password', resetPassword);
+ 
 
 export default authRouter;
