683dd44e7cc3759c28e8a859486c22290f430c60
nandani-g <nandaniigupta02@gmail.com>
2026-08-10 23:08:39 +0530

    Connect Login page to auth context and API

--- client/frontend/src/pages/Login.jsx
+++ client/frontend/src/pages/Login.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import { AppContent } from '../context/AppContext'
+import { toast } from 'react-toastify'
 
 const Login = () => {
+
+// before using this appcontent , we should add in main.jsx file because - thenn we get support of app context in this react application so that we can excess any data  from the context to any other component
+  const navigate = useNavigate()
+
+  // we will get the backend URL from our context so 
+  // to get the data from the context we will use content hook
+
+  const {backendUrl, setIsLoggedin} = useContext(AppContent)
 
   const [state, setState] = useState('Sign Up')
 
@@ -9,35 +21,82 @@ const Login = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const onSubmitHandler = async (e)=>{
+    try {
+      e.preventDefault();
+
+      if(state === 'Sign Up'){
+// in this we call api for registration api
+// to send cookies we have to make credential true for this API request 
+
+
+        axios.defaults.withCredentials = true;
+
+      const {data} =   await axios.post(backendUrl + '/api/auth/register', {name, email, password })
+        // mean user is loggedin
+      if(data.success){
+        setIsLoggedin(true)
+        navigate('/')
+      } else{
+        toast.error(data.message)
+      }
+ 
+      } else {
+         const {data} =   await axios.post(backendUrl + '/api/auth/login', {email, password })
+        // hit the login api
+      if(data.success){
+        setIsLoggedin(true)
+        navigate('/')
+      } else{
+        toast.error(data.message)
+      }
+
+
+      }
+    } catch (error) {
+
+        toast.error(data.message)
+      
+    }
+  }
 
 
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
-      <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+      <img onClick={()=> navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
       <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
         <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create account' : 'Login'}</h2>
 
         <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}</p>
 
-        <form>
+      {/* add onclick property so that whenever we submit the form it make an accounnt and logging with that name, email and password */}
+        <form onClick={onSubmitHandler}>
           {state === 'Sign Up' && (
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
               <img src={assets.person_icon} alt="" />
-              <input className='bg-transparent outline-none' type="text" placeholder='Full Name' required/>
+              <input onChange = {e => setName(e.target.value)} 
+              value = {name} 
+              className='bg-transparent outline-none' type="text" placeholder='Full Name' required/>
             </div>
           )}
 
           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
             <img src={assets.mail_icon} alt="" />
-            <input className='bg-transparent outline-none' type="email" placeholder='Email id' required/>
+            <input
+             onChange = {e => setEmail(e.target.value)} 
+              value = {email} 
+             className='bg-transparent outline-none' type="email" placeholder='Email id' required/>
           </div>
 
           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
             <img src={assets.lock_icon} alt="" />
-            <input className='bg-transparent outline-none' type="password" placeholder='Password' required/>
+            <input 
+             onChange = {e => setPassword(e.target.value)} 
+              value = {password} 
+               className='bg-transparent outline-none' type="password" placeholder='Password' required/>
           </div>
 
-          <p className='mb-4 text-indigo-500 cursor-pointer'>Forgot password?</p>
+          <p onClick={()=>navigate('/reset-password')} className='mb-4 text-indigo-500 cursor-pointer'>Forgot password?</p>
 
           <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium'>{state}</button>
         </form>
