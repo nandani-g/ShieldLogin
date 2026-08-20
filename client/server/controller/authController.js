8ad250e1175b4cc07a955aebf3e055eb89ae48b2
nandani-g <nandaniigupta02@gmail.com>
2026-08-20 21:10:36 +0530

    Update auth controller to use email templates for reset password

--- client/server/controller/authController.js
+++ client/server/controller/authController.js
@@ -5,6 +5,8 @@ import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 
 import transporter from '../config/nodemailer.js'
+
+import {PASSWORD_RESET_TEMPLATE, EMAIL_VERIFY_TEMPLATE} from '../config/emailTemplate.js';
 // generated a token for authecation
 
 
@@ -134,7 +136,8 @@ export const sendVerifyOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: 'Account Verification OTP',
-      text: `Your OTP is ${otp}. Verify your account using this OTP.`
+      // text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+      html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}", otp).replace("{{email}}",user.email)
     }
 
     await transporter.sendMail(mailOptions);
@@ -242,8 +245,9 @@ export const sendResetOtp = async (req, res)=> {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: 'Password Reset OTP',
-      text: `Your OTP for resetting your password is ${otp}.
-      Use this OTP to proceed with resetting your password.`
+      // text: `Your OTP for resetting your password is ${otp}.
+      // Use this OTP to proceed with resetting your password.`,
+      html: PASSWORD_RESET_TEMPLATE.replace("{{otp}}",otp).replace("{{email}}",user.email)
     }
 
     await transporter.sendMail(mailOptions);
